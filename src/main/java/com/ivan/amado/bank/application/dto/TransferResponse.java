2acf609680b0dec7ac2b76b064f871f756966771
IVAN AMADO CARDOSO <peacevan@hotmail.com>
2026-05-28 00:08:33 -0300

    feat(transfer): add REST controller and DTOs for transfer with controller tests

---
+++ src/main/java/com/ivan/amado/bank/application/dto/TransferResponse.java
@@ -0,0 +1,29 @@
+package com.ivan.amado.bank.application.dto;
+
+public class TransferResponse {
+    private String status;
+    private String message;
+
+    public TransferResponse() {}
+
+    public TransferResponse(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
